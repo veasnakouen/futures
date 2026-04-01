58e78e3a4202e089728253e208ef8c2097663ba5
veasna koeun <veasnakoeun@gmail.com>
2026-04-01 09:41:36 +0700

    * 'Line 55: Replaced 'HttpContext.Current' with 'HttpContextWrapper.Current' to address the breaking change in .NET 10.0 where 'HttpContext.Current' is no longer available. 'HttpContextWrapper' provides access to the current HTTP context in newer frameworks.' in file 'Controllers\Api\SocialCareController.cs'

--- Controllers/Api/SocialCareController.cs
+++ Controllers/Api/SocialCareController.cs
@@ -52,7 +52,7 @@ namespace MtpApp.Controllers.Api
 
               var socialCareDto = new SocialCareDto()
               {
-                  ClientId = int.Parse(HttpContext.Current.Request.Form["ClientId"]),
+                  ClientId = int.Parse(HttpContextWrapper.Current.Request.Form["ClientId"]),
                   SocialSupportNeeded = Boolean.Parse(HttpContext.Current.Request.Form["socialsupportNeeded"]),
                   MeetingFuture = Boolean.Parse(HttpContext.Current.Request.Form["Meetingfuture"]),
                   Problem = Boolean.Parse(HttpContext.Current.Request.Form["problems"])
