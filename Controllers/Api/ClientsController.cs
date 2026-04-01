116e6d4cb1c3ceae6d8f4b05caae90791f6bdc7c
veasna koeun <veasnakoeun@gmail.com>
2026-04-01 09:30:27 +0700

    * 'Line 129: Replaced 'HttpContext.Current' with 'System.Web.HttpContext.Current' to ensure the correct static property is accessed, as 'Current' is only available on 'System.Web.HttpContext'.' in file 'Controllers\Api\ClientsController.cs'

--- Controllers/Api/ClientsController.cs
+++ Controllers/Api/ClientsController.cs
@@ -126,7 +126,7 @@ namespace MtpApp.Controllers.Api
                 Branch = user.Branch,
                 ClientCode = System.Web.HttpContext.Current.Request.Form["ClientCode"],
                 FirstName = System.Web.HttpContext.Current.Request.Form["FirstName"],
-                LastName = HttpContext.Current.Request.Form["LastName"],
+                LastName = System.Web.HttpContext.Current.Request.Form["LastName"],
                 Gender = HttpContext.Current.Request.Form["Gender"],
                 DateOfBirth = DateTime.ParseExact(HttpContext.Current.Request.Form["DateOfBirth"], "MM/dd/yyyy", null),
                 ContactPhone = HttpContext.Current.Request.Form["ContactPhone"],
